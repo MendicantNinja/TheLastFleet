ac78443e76d52a8fbd2119bf24ea78eb6ce79544
turtle-water <160274904+turtle-water@users.noreply.github.com>
2024-09-28 19:11:06 -0400

    Initial implementation of AI pathfinding and movement w/o rotation

---
+++ Scripts/ship.gd
@@ -0,0 +1,72 @@
+extends RigidBody2D
+class_name Ship
+
+const SPEED = 2000.0
+@onready var ShipNavigationAgent = $ShipNavigationAgent
+var acceleration: Vector2 = Vector2.ZERO
+var angular_acceleration: float = 0.0
+var stop_ship: bool = false
+var manual_control: bool = false
+var ship_select: bool = false
+
+func _ready() -> void:
+	pass
+
+# Any generic input event.
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		if event.pressed and event.button_mask == MOUSE_BUTTON_MASK_LEFT and ship_select:
+			ShipNavigationAgent.set_target_position(event.global_position)  # move selected ship at event position
+		elif event.pressed and event.button_mask == MOUSE_BUTTON_MASK_RIGHT and ship_select:
+			ship_select = false # deselect ship by right clicking anywhere
+	
+	if event is InputEventKey:
+		pass
+
+# When the player interacts with the ship via mouse.
+func _on_input_event(viewport: Viewport, event: InputEvent, shape_idx: int) -> void:
+	if event is InputEventMouseButton:
+		if event.pressed and event.button_mask == MOUSE_BUTTON_MASK_LEFT and not ship_select:
+			ship_select = true # select ship
+		elif event.pressed and event.button_mask == MOUSE_BUTTON_MASK_LEFT and ship_select:
+			ship_select = false # deselect ship
+
+func _physics_process(delta: float) -> void:
+	if NavigationServer2D.map_get_iteration_id(ShipNavigationAgent.get_navigation_map()) == 0:
+		return
+	
+	if ShipNavigationAgent.is_navigation_finished() and stop_ship:
+		stop_ship = false
+	
+	var velocity: Vector2 = Vector2.ZERO
+	if not ShipNavigationAgent.is_navigation_finished() and not stop_ship:
+		var ship_position: Vector2 = transform.get_origin()
+		var next_path_position: Vector2 = ShipNavigationAgent.get_next_path_position()
+		velocity = ship_position.direction_to(next_path_position) * SPEED * delta
+	
+	acceleration += velocity - linear_velocity
+
+func _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
+	var force: Vector2 = mass * acceleration
+	if force.abs().floor() != Vector2.ZERO:
+		state.apply_central_force(force)
+	
+	if stop_ship:
+		# decrease linear velocity to converge acceleration to zero, thus
+		# converging force to zero
+		var weight: float = 1.0 / linear_velocity.length()
+		linear_velocity += lerp(Vector2.ZERO, -linear_velocity, state.step)
+	elif not ShipNavigationAgent.is_navigation_finished() and not stop_ship:
+		var ship_position: Vector2 = state.transform.get_origin()
+		var final_position: Vector2 = ShipNavigationAgent.get_target_position()
+		var distance_between_pos: float = final_position.distance_to(ship_position)
+		var desired_distance: float = ShipNavigationAgent.get_target_desired_distance()
+		var lin_vel_magnitude: float = linear_velocity.length()
+		if distance_between_pos < lin_vel_magnitude and distance_between_pos > desired_distance:
+			stop_ship = true 
+	
+	# use apply_impulse for one-shot calculations for collisions
+	# its time-independent hence why its a one-shot
+
+func _on_ShipNavAgent_velocity_computed(safe_velocity: Vector2) -> void:
+	linear_velocity = safe_velocity
