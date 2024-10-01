3a833ab7673c9dbf179022e5d08d13921fc43c9b
turtle-water <160274904+turtle-water@users.noreply.github.com>
2024-09-30 22:02:39 -0400

    WIP proper avoidance scripting

--- Scripts/ship.gd
+++ Scripts/ship.gd
@@ -5,8 +5,8 @@ const SPEED = 3000.0
 @onready var ShipNavigationAgent = $ShipNavigationAgent
 var acceleration: Vector2 = Vector2.ZERO
 var avoidance_velocity: Vector2 = Vector2.ZERO
+var target_position: Vector2 = Vector2.ZERO
 var movement_delta: float = 0.0
-var manual_control: bool = false
 var ship_select: bool = false
 
 func _ready() -> void:
@@ -16,7 +16,8 @@ func _ready() -> void:
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
 		if event.pressed and event.button_mask == MOUSE_BUTTON_MASK_LEFT and ship_select:
-			ShipNavigationAgent.set_target_position(event.global_position)  # move selected ship at event position
+			target_position = event.global_position
+			ShipNavigationAgent.set_target_position(target_position)  # move selected ship at event position
 		elif event.pressed and event.button_mask == MOUSE_BUTTON_MASK_RIGHT and ship_select:
 			ship_select = false # deselect ship by right clicking anywhere
 	
@@ -35,23 +36,27 @@ func _physics_process(delta: float) -> void:
 	if NavigationServer2D.map_get_iteration_id(ShipNavigationAgent.get_navigation_map()) == 0:
 		return
 	
+	var ship_position: Vector2 = transform.get_origin()
+	var raycast_result: Dictionary = {}
 	var velocity = Vector2.ZERO
 	movement_delta = SPEED * delta
-	if not ShipNavigationAgent.is_navigation_finished():
-		var ship_position: Vector2 = transform.get_origin()
+	if target_position != Vector2.ZERO:
+		var space_state = get_world_2d().direct_space_state
+		var query = PhysicsRayQueryParameters2D.create(global_position, target_position, 3, [self])
+		raycast_result = space_state.intersect_ray(query)
+	
+	if not raycast_result.is_empty() and not ShipNavigationAgent.is_navigation_finished():
+		var collider_instance: Node = instance_from_id(raycast_result["collider_id"])
+		var collider_navagent: NavigationAgent2D = collider_instance.find_child("NavigationAgent2D")
+		var collider_center: Vector2 = raycast_result["collider"].global_position
+		var navagent_radius: int = collider_navagent.radius
+		
+		pass
+	elif not ShipNavigationAgent.is_navigation_finished():
 		var next_path_position: Vector2 = ShipNavigationAgent.get_next_path_position()
 		var direction_to_path: Vector2 = ship_position.direction_to(next_path_position)
 		velocity = direction_to_path * movement_delta
 		
-		if avoidance_velocity != Vector2.ZERO:
-			var direction_to_repath: Vector2 = velocity.direction_to(avoidance_velocity)
-			var test_repath_x: int = direction_to_path.x - direction_to_repath.x
-			var test_repath_y: int = direction_to_path.y - direction_to_repath.y
-			if test_repath_x == 0.0:
-				velocity.x += avoidance_velocity.x
-			if test_repath_y == 0.0:
-				velocity.y += avoidance_velocity.y
-				
 		var transform_look_at: Transform2D = transform.looking_at(next_path_position)
 		transform = transform.interpolate_with(transform_look_at, delta)
 	
@@ -71,6 +76,7 @@ func _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
 	# its time-independent hence why its a one-shot
 
 func _on_ShipNavigationAgent_velocity_computed(safe_velocity):
-	avoidance_velocity = Vector2.ZERO
-	if safe_velocity != Vector2.ZERO:
-		avoidance_velocity = safe_velocity
+	pass
+	#avoidance_velocity = Vector2.ZERO
+	#if safe_velocity != Vector2.ZERO:
+		#avoidance_velocity = safe_velocity
