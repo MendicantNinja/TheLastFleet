17aab359e8ae0ee437a8ec68f009369258b85cc9
MendicantNinja <mendicantninja@gmail.com>
2024-10-01 00:41:09 -0400

    Basic Agent Avoidance Patch
    Includes intermediate pathing fix amend.

--- Scripts/ship.gd
+++ Scripts/ship.gd
@@ -6,6 +6,11 @@ const SPEED = 3000.0
 var acceleration: Vector2 = Vector2.ZERO
 var avoidance_velocity: Vector2 = Vector2.ZERO
 var target_position: Vector2 = Vector2.ZERO
+
+# Used for intermediate pathing around dynamic agents
+var final_target_position: Vector2 = Vector2.ZERO
+var intermediate_pathing: bool = false
+
 var movement_delta: float = 0.0
 var ship_select: bool = false
 
@@ -16,6 +21,7 @@ func _ready() -> void:
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
 		if event.pressed and event.button_mask == MOUSE_BUTTON_MASK_LEFT and ship_select:
+			intermediate_pathing = false
 			target_position = event.global_position
 			ShipNavigationAgent.set_target_position(target_position)  # move selected ship at event position
 		elif event.pressed and event.button_mask == MOUSE_BUTTON_MASK_RIGHT and ship_select:
@@ -35,30 +41,63 @@ func _on_input_event(viewport: Viewport, event: InputEvent, shape_idx: int) -> v
 func _physics_process(delta: float) -> void:
 	if NavigationServer2D.map_get_iteration_id(ShipNavigationAgent.get_navigation_map()) == 0:
 		return
-	
+
+#collider: The colliding object.
+#
+#collider_id: The colliding object's ID.
+#
+#normal: The object's surface normal at the intersection point, or Vector2(0, 0) if the ray starts inside the shape and PhysicsRayQueryParameters2D.hit_from_inside is true.
+#
+#position: The intersection point.
+#
+#rid: The intersecting object's RID.
+#
+#shape: The shape index of the colliding shape.
+
+
 	var ship_position: Vector2 = transform.get_origin()
 	var raycast_result: Dictionary = {}
 	var velocity = Vector2.ZERO
 	movement_delta = SPEED * delta
-	if target_position != Vector2.ZERO:
-		var space_state = get_world_2d().direct_space_state
-		var query = PhysicsRayQueryParameters2D.create(global_position, target_position, 3, [self])
+	
+	# Do these two branch statements when first detecting a collision. 
+	if intermediate_pathing == false and target_position != Vector2.ZERO:
+		print("Raycast shootout")
+		var space_state: PhysicsDirectSpaceState2D = get_world_2d().direct_space_state
+		var query: PhysicsRayQueryParameters2D = PhysicsRayQueryParameters2D.create(global_position, target_position, 1, [self])
 		raycast_result = space_state.intersect_ray(query)
 	
-	if not raycast_result.is_empty() and not ShipNavigationAgent.is_navigation_finished():
+	if intermediate_pathing == false and not raycast_result.is_empty() and not ShipNavigationAgent.is_navigation_finished():
+		print("Raycast shootout calculate and set new intermediate path")
 		var collider_instance: Node = instance_from_id(raycast_result["collider_id"])
-		var collider_navagent: NavigationAgent2D = collider_instance.find_child("NavigationAgent2D")
-		var collider_center: Vector2 = raycast_result["collider"].global_position
+		#if collider_instance is 
+		var collider_navagent: NavigationAgent2D = collider_instance.find_child("ShipNavigationAgent")
+		if collider_navagent == null:
+			return
+		var collider_center: Vector2 = collider_instance.position
 		var navagent_radius: int = collider_navagent.radius
-		
-		pass
+		intermediate_pathing = true
+		final_target_position = target_position
+		target_position = Vector2(collider_center.x - navagent_radius/1.3, collider_center.y)
+		ShipNavigationAgent.set_target_position(target_position)
+	
+	# Do this after finishing the intermediate pathing.
+	elif intermediate_pathing == true and ShipNavigationAgent.is_navigation_finished():
+		print("Intermediate pathing finished")
+		target_position = final_target_position
+		ShipNavigationAgent.set_target_position(target_position)
+		intermediate_pathing = false
+	
+	# Normal Pathing
 	elif not ShipNavigationAgent.is_navigation_finished():
+		print("Pathing normally as navigation is not finished")
 		var next_path_position: Vector2 = ShipNavigationAgent.get_next_path_position()
 		var direction_to_path: Vector2 = ship_position.direction_to(next_path_position)
 		velocity = direction_to_path * movement_delta
 		
 		var transform_look_at: Transform2D = transform.looking_at(next_path_position)
 		transform = transform.interpolate_with(transform_look_at, delta)
+	
 	
 	acceleration += velocity - linear_velocity
 	
