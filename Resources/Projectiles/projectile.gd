f360da9ac28a3349aaaf56980e557541c394cd39
MendicantNinja <mendicantninja@gmail.com>
2024-10-08 15:31:08 -0400

    Quick Fix 2

--- Resources/Projectiles/projectile.gd
+++ Resources/Projectiles/projectile.gd
@@ -4,12 +4,8 @@ class_name Projectile
 # Stats
 @export var weapon: Weapon 
 #@export var hit_noise: Resource
-# What happens when the projectile hits? Grab damage_per_shot from weapon and collided_object_instance for the enemy to calculate. Play a noise.
-func hit() -> void:
-	if weapon == null:
-		push_error("No weapon detected for projectile. res://Resources/Projectiles/projectile.gd ")
-		return
-	pass
+
+
 
 
 func _ready():
--- Scripts/GameObjectScripts/projectile_scene.gd
+++ Scripts/GameObjectScripts/projectile_scene.gd
@@ -4,7 +4,12 @@ class_name ProjectileScene
 # Stats
 @export var projectile: Projectile
 
-
+# What happens when the projectile hits? Grab damage_per_shot from projectile.weapon and collided_object_instance for the enemy to calculate. Play a noise from projectile.noise.
+func hit() -> void:
+	if projectile == null or projectile.weapon == null:
+		push_error("No weapon and/or projectile detected for projectile scene. res://Resources/Projectiles/projectile.gd ")
+		return
+	pass
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
