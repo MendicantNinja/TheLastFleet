322d00d2112c30f4978764ca09c105177c05b4e7
turtle-water <160274904+turtle-water@users.noreply.github.com>
2025-02-16 16:53:42 -0500

    before a power outage hits

--- Scripts/GameObjectScripts/CombatAI/BehaviorTree/BehaviorPool/group_units.gd
+++ Scripts/GameObjectScripts/CombatAI/BehaviorTree/BehaviorPool/group_units.gd
@@ -1,67 +1,37 @@
 extends LeafAction
 
+var visited_units: Array = []
+var strength_modifier: float = 1.0
+
 func tick(agent: Admiral, blackboard: Blackboard) -> int:
 	if Engine.get_physics_frames() % 240 != 0:
 		return FAILURE
 	
-	var available_units: Array = get_tree().get_nodes_in_group(&"enemy")
-	var lone_units: Array = []
-	var unit_ranks: Dictionary = {}
-	for unit in available_units:
-		var floor_inf: float = floor(unit.approx_influence)
-		if unit.group_name.is_empty():
-			lone_units.append(unit)
-		if not unit_ranks.has(floor_inf):
-			unit_ranks[floor_inf] = []
-		if unit not in unit_ranks[floor_inf]:
-			unit_ranks[floor_inf].append(unit)
+	var player_total_strength: float = 0.0
+	var admiral_total_strength: float = 0.0
+	var battle_agents: Array = get_tree().get_nodes_in_group(&"agent")
+	var available_player_units: int = 0
+	var available_enemy_units: int = 0
+	for unit: Ship in battle_agents:
+		if unit == null:
+			print("The destroyed unit %s still persists in memory in group_units.gd." % [unit.name])
+			continue
+		var floor_inf: float = floor(abs(unit.approx_influence))
+		if unit.is_friendly == true:
+			player_total_strength += floor_inf
+			available_player_units += 1
+		else:
+			admiral_total_strength += floor_inf
+			available_enemy_units += 1
 	
-	if lone_units.is_empty():
-		return FAILURE
-	
-	var enemy_units: Array = get_tree().get_nodes_in_group(&"friendly")
-	var enemy_ranks: Dictionary = {}
-	var average_enemy_strength: float = 0.0
-	for unit in enemy_units:
-		var floor_inf: float = ceil(unit.approx_influence)
-		if not enemy_ranks.has(floor_inf):
-			enemy_ranks[floor_inf] = []
-		if unit not in enemy_ranks[floor_inf]:
-			enemy_ranks[floor_inf].append(unit)
-		average_enemy_strength += floor_inf
-	
-	average_enemy_strength /= enemy_units.size()
-	average_enemy_strength = floor(average_enemy_strength)
-	
-	var weighted_ranks: Dictionary = {}
-	var rank_sizes: Array = []
-	for rank in unit_ranks.keys():
-		var units = unit_ranks[rank]
-		var weigh_rank: float = -floor(rank / average_enemy_strength)
-		weighted_ranks[weigh_rank] = units
-		rank_sizes.append(units.size())
-	
-	var unit_positions: Dictionary = {}
-	for unit in available_units:
-		if unit in lone_units:
-			unit_positions[unit.global_position] = unit
-	var geo_median: Vector2 = globals.geometric_median_of_objects(unit_positions.keys())
-	
-	if agent.heuristic_strat == globals.Strategy.DEFENSIVE:
-		var group_leader: Ship = globals.find_unit_nearest_to_median(geo_median, unit_positions)
-		var group_name: StringName = agent.group_key_prefix + str(agent.iterator)
-		agent.iterator += 1
-		get_tree().call_group(&"enemy", "group_add", group_name)
-		group_leader.set_group_leader(true)
-		agent.awaiting_orders.append(group_name)
-		agent.queue_orders[agent.posture_key] = agent.heuristic_strat
-	elif agent.heuristic_strat == globals.Strategy.OFFENSIVE:
-		var ratio: int = available_units.size() / enemy_units.size()
-		var remainder: int = available_units.size() % enemy_units.size()
-		for i in range(0, ratio, 1):
-			pass
-		
-		pass
-	
-	
-	return SUCCESS
+	var relative_strength: float = admiral_total_strength / player_total_strength
+	var unit_ratio: int = available_enemy_units / available_player_units
+	var remainder: int = available_enemy_units % available_player_units
+	var average_unit_strength: float = admiral_total_strength / available_enemy_units
+	var strength_coefficient: float = strength_modifier
+	var base_group_strength: float = relative_strength * average_unit_strength
+	if remainder > 1 and remainder != available_enemy_units:
+		var relative_group_size: int = (available_player_units - remainder) / unit_ratio
+		strength_coefficient *= relative_group_size
+	var adjusted_group_strength: float = base_group_strength * strength_coefficient
+	return FAILURE
