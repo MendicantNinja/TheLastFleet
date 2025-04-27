927fc9d1fe2b687c2c9175efb44dbeee00f9e78d
MendicantNinja <113736172+MendicantNinja@users.noreply.github.com>
2025-04-27 01:00:23 -0400

    IdentifyNearbyUnits

--- Scripts/GameObjectScripts/CombatAI/BehaviorTreeC#/BehaviorPool/IdentifyNearbyUnits.cs
+++ Scripts/GameObjectScripts/CombatAI/BehaviorTreeC#/BehaviorPool/IdentifyNearbyUnits.cs
@@ -1,34 +1,113 @@
 using Godot;
 using System;
+using System.Linq;
 using System.Collections.Generic;
-
+using System.Numerics;
+using Vector2 = System.Numerics.Vector2;
 public partial class IdentifyNearbyUnits : Action
 {
-    public override NodeState Tick(Node agent)
-    {
+	public override NodeState Tick(Node agent)
+	{   
+		
+		if (Engine.GetPhysicsFrames() % 120 != 0)
+			return NodeState.FAILURE;
+		SteerData steer_data = (SteerData)agent.Get("SteerData");
+		ShipWrapper ship_wrapper = (ShipWrapper) agent.Get("ShipWrapper");
 
-        SteerData steer_data = (SteerData)agent.Get("SteerData");
-        ShipWrapper ship_wrapper = (ShipWrapper) agent.Get("ShipWrapper");
+		
+		List<String> nearby_enemy_groups = new List<String>();
+		List<RigidBody2D> idle_neighbors = new List<RigidBody2D>();
+		List<String> neighbor_groups = new List<String>();
 
-        
-        List<string> nearby_enemy_groups = new List<string>();
-        List<RigidBody2D> idle_neighbors = new List<RigidBody2D>();
-        List<string> neighbor_groups = new List<string>();
+		foreach (Vector2I cell in ship_wrapper.RegistryNeighborhood)
+		{
+			if (ImapManager.Instance.RegistryMap.ContainsKey(cell)) 
+				continue;
+			foreach (RigidBody2D ship in ImapManager.Instance.RegistryMap[cell])
+			{ 
+				ShipWrapper unit = (ShipWrapper) ship.Get("ShipWrapper");
+			   
+				if (unit == null)
+					continue;
+				if (unit.GroupName == ship_wrapper.GroupName && unit.GroupName.Count() != 0 && ship_wrapper.GroupName.Count() != 0)
+					continue;
+					if (unit.IsFriendly == ship_wrapper.IsFriendly && unit.GroupName.Count() == 0)
+				{
+					idle_neighbors.Add(ship);
+				}
+				if (unit.IsFriendly != ship_wrapper.IsFriendly && unit.GroupName.Count() != 0 && !nearby_enemy_groups.Contains(unit.GroupName))
+				{
+					nearby_enemy_groups.Add(unit.GroupName);
+				}
+				if (unit.IsFriendly == ship_wrapper.IsFriendly && unit.GroupName.Count() != 0 && !neighbor_groups.Contains(unit.GroupName))
+				{
+					neighbor_groups.Add(unit.GroupName);
+				}
+			}
+		}
+		List<string> nearby_attackers = new List<string>();
+		foreach (RigidBody2D ship in ship_wrapper.TargetedBy)
+		{
+			if (ship == null)
+				continue;
 
-        foreach (Vector2I cell in ship_wrapper.RegistryNeighborhood)
-        {
-            if (ImapManager.Instance.RegistryMap.ContainsKey(cell)) continue;
-            // finish loop
-        }
+			ShipWrapper unit = (ShipWrapper)ship.Get("ShipWrapper");
+			if (!nearby_attackers.Contains(unit.GroupName))
+			{
+				nearby_attackers.Add(unit.GroupName);
+			}
+		}
+		foreach (RigidBody2D ship in GetTree().GetNodesInGroup(ship_wrapper.GroupName))
+		{
+			if (ship == null)
+				continue;
 
-        foreach (RigidBody2D unit in ship_wrapper.TargetedBy)
-        {
-            // finish loop here
-        }
+			ShipWrapper unit = (ShipWrapper)ship.Get("ShipWrapper");
+			foreach (RigidBody2D attacker_ship in unit.TargetedBy)
+			{
+				if (attacker_ship == null)
+					continue;
 
-    
-        // all the "nearby" "idle" etc stuff should already exist in the ShipWrapper
-        return NodeState.FAILURE;
-    }
+				ShipWrapper attacker = (ShipWrapper)attacker_ship.Get("ShipWrapper");
+				if (!nearby_attackers.Contains(attacker.GroupName))
+				{
+					nearby_attackers.Add(attacker.GroupName);
+				}
+			}
+		}
+
+		List<string> available_neighbor_groups = new List<string>();
+		foreach (string group_name in neighbor_groups)
+		{
+			int availability_count = 0;
+			var group = GetTree().GetNodesInGroup(group_name);
+
+			foreach (RigidBody2D ship in group)
+			{
+				if (ship == null)
+					continue;
+
+				ShipWrapper unit = (ShipWrapper)ship.Get("ShipWrapper");
+				SteerData unit_steering = (SteerData)ship.Get("SteerData");
+
+				if (unit.TargetedUnits.Count() == 0 && unit_steering.TargetPosition == Vector2.Zero && ship_wrapper.CombatFlag == false)
+				{
+					availability_count += 1;
+				}
+			}
+
+			if (availability_count == group.Count)
+			{
+				available_neighbor_groups.Add(group_name);
+			}
+		}
+		ship_wrapper.AvailableNeighborGroups = available_neighbor_groups;
+		ship_wrapper.NearbyEnemyGroups = nearby_enemy_groups;
+		ship_wrapper.IdleNeighbors = idle_neighbors;
+		ship_wrapper.NeighborGroups = neighbor_groups;
+		ship_wrapper.NearbyAttackers = nearby_attackers;
+
+		return NodeState.FAILURE;
+	}
 
 }
--- Scripts/GameObjectScripts/CombatAI/BehaviorTreeC#/BehaviorPool/ThreatDetection.cs
+++ Scripts/GameObjectScripts/CombatAI/BehaviorTreeC#/BehaviorPool/ThreatDetection.cs
@@ -1,23 +1,115 @@
 using Godot;
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using Vector2 = System.Numerics.Vector2;
 
 public partial class ThreatDetection : Action
 {
-    string true_name = "";
-    string tmp_name = "threat detect ";
+	string true_name = "";
+	string tmp_name = "threat detect ";
 
-    public override NodeState Tick(Node agent)
-    {
-        ShipWrapper ship_wrapper = (ShipWrapper)agent.Get("ShipWrapper");
-        SteerData steer_data = (SteerData)agent.Get("SteerData");
+	public override NodeState Tick(Node agent)
+	{
+		ShipWrapper ship_wrapper = (ShipWrapper)agent.Get("ShipWrapper");
+		SteerData steer_data = (SteerData)agent.Get("SteerData");
+        Node globals = GetTree().Root.GetNode("globals");
+        if (Engine.GetPhysicsFrames() % 120 != 0 || ship_wrapper.RegistryCell == Godot.Vector2I.One || steer_data.TargetPosition != Vector2.Zero)
+        {
+            return NodeState.FAILURE;
+        }
+        else if (ship_wrapper.CombatFlag || ship_wrapper.VentFluxFlag || ship_wrapper.FallbackFlag || ship_wrapper.RetreatFlag)
+        {
+            return NodeState.FAILURE;
+        }
+
+          // Initialize group names based on the ship's friendly status
+        if (true_name.Count() == 0 && ship_wrapper.IsFriendly)
+        {
+            true_name = "Temporary Friendly Group ";
+            tmp_name += "tmp friendly";
+        }
+        else if (true_name.Count() == 0 && !ship_wrapper.IsFriendly)
+        {
+            true_name = "Temporary Enemy Group ";
+            tmp_name += "tmp enemy";
+        }
+
+        // Check if any unit in the group has a non-zero target position or is in combat
+        // Ported
+        foreach (RigidBody2D ship in GetTree().GetNodesInGroup(ship_wrapper.GroupName))
+        {
+            ShipWrapper unit = (ShipWrapper)ship.Get("ShipWrapper");
+            SteerData unit_steering = (SteerData)ship.Get("SteerData");
+            if (unit == null) continue;
+            if (unit_steering.TargetPosition != Vector2.Zero || ship_wrapper.CombatFlag)
+                return NodeState.FAILURE;
+        }
+
+        List<String> attacker_groups = ship_wrapper.NearbyAttackers;
+        if (attacker_groups.Count == 0)
+            return NodeState.FAILURE;
+
+        List<RigidBody2D> idle_neighbors = ship_wrapper.IdleNeighbors;
+        List<String> available_neighbor_groups = ship_wrapper.AvailableNeighborGroups;
 
         // You will need to know this is how we currently access globals
-        // Node globals = GetTree().Root.GetNode("globals");
-        // Call its functions like this
-	    // globals.Call("generate_group_target_positions", agent);
+		// Node globals = GetTree().Root.GetNode("globals");
+		// Call its functions like this
+		// globals.Call("generate_group_target_positions", agent);
 
 
-        return NodeState.FAILURE;
-    }
+        if (ship_wrapper.GroupName.Count() == 0 && available_neighbor_groups.Count == 0 && idle_neighbors.Count > 0)
+        {   // I had to use Godot's Vector2 to compare to the ships global_position property. 
+            Dictionary<Godot.Variant, RigidBody2D> unit_positions = new Dictionary<Variant, RigidBody2D>();
+            foreach (RigidBody2D ship in idle_neighbors)
+            {
+                ship.AddToGroup(tmp_name);
+                unit_positions[ship.Get("global_position")] = ship;
+            }
+
+            Vector2 geo_median = globals.Call("geometric_median_of_objects", unit_positions.Keys.ToList());
+            ShipWrapper leader = globals.find_unit_nearest_to_median(geo_median, unit_positions);
+            string new_group_name = true_name + agent.Name;
+            GetTree().CallGroup(tmp_name, "group_add", new_group_name);
+            GetTree().CallGroup(tmp_name, "group_remove", tmp_name);
+            leader.SetGroupLeader(true);
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        Dictionary<float, string> nearby_group_strength = new Dictionary<float, string>();
+        foreach (String group_name in available_neighbor_groups)
+        {
+            
+            float relative_group_strength = 0.0f;
+            foreach (RigidBody2D ship in GetTree().GetNodesInGroup(group_name))
+            {
+                ShipWrapper unit = (ShipWrapper)ship.Get("ShipWrapper");
+                if (unit == null) continue;
+                relative_group_strength += unit.ApproxInfluence;
+            }
+            if (relative_group_strength == 0.0f)
+                continue;
+            nearby_group_strength[relative_group_strength + agent_group_strength] = group_name;
+        }
+
+
+		return NodeState.FAILURE;
+	}
 
 }
