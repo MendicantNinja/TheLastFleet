7d7c5f6e13db715981076968293ab785e35e99ba
MendicantNinja <113736172+MendicantNinja@users.noreply.github.com>
2025-05-01 20:22:47 -0400

    Update FindGoal.cs

--- Scripts/GameObjectScripts/CombatAI/BehaviorTreeC#/BehaviorPool/FindGoal.cs
+++ Scripts/GameObjectScripts/CombatAI/BehaviorTreeC#/BehaviorPool/FindGoal.cs
@@ -1,9 +1,10 @@
 using Godot;
 using InfluenceMap;
 using System;
+using System.Collections.Generic;
 
 public partial class FindGoal : Action
-{
+{   const int radius = 30;
     bool is_friendly = false;
     public override NodeState Tick(Node agent)
     {
@@ -21,13 +22,64 @@ public partial class FindGoal : Action
 
         if (ship_wrapper.CombatGoal == Globals.Goal.SKIRMISH && ship_wrapper.GoalFlag == true && ship_wrapper.TargetedUnits.Count == 0)
         {
-            agent.Set("combat_flag", false);
+            ship_wrapper.SetGoalFlag(false);
             GetTree().CallGroup(ship_wrapper.GroupName, "set_goal_flag", false);
         }
 
         if (ship_wrapper.GoalFlag == true) return NodeState.SUCCESS;
 
         SteerData steer_data = (SteerData)agent.Get("SteerData");
+
+        Imap working_map = null;
+		if (working_map == null)
+		{
+			working_map = new Imap(radius, radius);
+		}
+        
+		ImapManager.Instance.GoalMap.AddIntoMap(working_map, ship_wrapper.ImapCell.Y, ship_wrapper.ImapCell.X);
+		//ship_wrapper.WorkingMap = working_map;
+		Dictionary<float, Vector2I> local_maximum = new Dictionary<float, Vector2I>();
+		int rows = working_map.MapGrid.GetLength(0); // Y
+		int cols = working_map.MapGrid.GetLength(1); // X
+		for (int m = 0; m < rows; m++)
+{
+			float maxVal = float.MinValue;
+			int n = -1;
+
+			for (int i = 0; i < cols; i++)
+			{
+				float val = working_map.MapGrid[m, i];
+				if (val > maxVal)
+				{
+					maxVal = val;
+					n = i;
+				}
+			}
+
+			if (maxVal <= 0.0f || n == -1)
+				continue;
+
+			Vector2I cell = new Vector2I(ship_wrapper.ImapCell.Y + m, ship_wrapper.ImapCell.X + n);
+			local_maximum[maxVal] = cell;
+		}
+
+		if (local_maximum.Count == 0)
+			return NodeState.FAILURE;
+
+		float highest_value = float.MinValue;
+		foreach (float key in local_maximum.Keys)
+		{
+			if (key > highest_value)
+				highest_value = key;
+		}
+
+		Vector2I cell_max = local_maximum[highest_value];
+		Vector2 target_position = new Vector2(
+			cell_max.Y * ImapManager.Instance.DefaultCellSize,
+			cell_max.X * ImapManager.Instance.DefaultCellSize
+		);
+
+		agent.Call("SetNavigationPosition", target_position);
 
 
         return NodeState.FAILURE;
