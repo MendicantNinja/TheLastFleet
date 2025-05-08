a1bbe7b73f7dd435a9374022d0b93037793f459e
MendicantNinja <113736172+MendicantNinja@users.noreply.github.com>
2025-05-08 16:58:03 -0400

    Playtest Ready?

--- Scripts/AutoloadScripts/data.gd
+++ Scripts/AutoloadScripts/data.gd
@@ -162,8 +162,8 @@ var loadout_dictionary: Dictionary = {
 			weapon_dictionary.get(weapon_enum.RAILGUN), 
 			weapon_dictionary.get(weapon_enum.COMBATLASER),
 			weapon_dictionary.get(weapon_enum.COMBATLASER), 
-			weapon_dictionary.get(weapon_enum.CBEAM), 
-			weapon_dictionary.get(weapon_enum.CBEAM),
+			weapon_dictionary.get(weapon_enum.COMBATLASER), 
+			weapon_dictionary.get(weapon_enum.COMBATLASER),
 			weapon_dictionary.get(weapon_enum.RAILGUN), 
 			weapon_dictionary.get(weapon_enum.RAILGUN),
 			]
@@ -175,8 +175,8 @@ var loadout_dictionary: Dictionary = {
 			]
 	},
 	ship_type_enum.ECLIPSE: {
-		loadout_enum.DEFAULT: [weapon_dictionary.get(weapon_enum.CBEAM), 
-			weapon_dictionary.get(weapon_enum.CBEAM), 
+		loadout_enum.DEFAULT: [weapon_dictionary.get(weapon_enum.COMBATLASER), 
+			weapon_dictionary.get(weapon_enum.COMBATLASER), 
 			]
 	}
 }
--- Scripts/OtherObjectScripts/fleet_deployment.gd
+++ Scripts/OtherObjectScripts/fleet_deployment.gd
@@ -38,8 +38,8 @@ func reset_deployment_position() -> void:
 	# 					   . . . . <- ending position
 	deployment_position.x = PlayableAreaBounds.shape.size.x/2 - deployment_spacing * 3 # 3+1+3 = 7 columns, start leftmost
 	deployment_position.y = PlayableAreaBounds.shape.size.y + deployment_spacing * 2 # Start topmost row.
-	if settings.debug_mode == true:
-		deployment_position.y = 5000 + deployment_spacing * 2
+	#if settings.debug_mode == true:
+		#deployment_position.y =  + deployment_spacing * 2
 	deployment_row = 0
 
 func on_icon_toggled(toggled_on: bool, this_icon: ShipIcon) -> void:
